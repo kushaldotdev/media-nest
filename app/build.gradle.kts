9d9a0f412792f129b338276e83ecf04928207638
kushaldotdev <bairagyakushal4@gmail.com>
2026-06-26 19:16:45 +0530

    Bump version to v1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.example.medianest"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
